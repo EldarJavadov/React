1a4cc4f6f4311285a8e318eb9c6e6e2dca414123
EldarJavadov <144907676+EldarJavadov@users.noreply.github.com>
2023-12-29 12:02:25 +0400

    Delete src directory

--- src/utils/api.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = {
-    baseUrl: 'https://dummyjson.com/',
-    productsUrl: 'https://dummyjson.com/products',
-    categoryUrl: 'https://dummyjson.com/products/categories'
-}
-
-export default apiUrl
